a542d73da3b2257121790a9a79dd380f70af4ab7
Jay Bednar <ironviralmedia@gmail.com>
2018-04-23 16:54:01 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'new post' do
 
-  it 'renders HTML in the /new template' do 
+  it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
   end 
